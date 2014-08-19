4e5a874009ea67f2dd75574945331a3235b9fc60
Anthony Hastings <anthony@rehabstudio.com>
2014-08-19 15:43:47 +0100

    - Correcting documentation of root gruntfile.

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
-    gulpfile.js
+    gruntfile.js
     ===========
     Rather than manage one giant configuration file responsible
     for creating multiple tasks, each task has been broken out into
