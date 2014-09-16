e414610b13dbe15e80d94e49333f0150e5177907
Anthony Hastings <anthony@rehabstudio.com>
2014-09-16 09:38:30 +0100

    - Adding Karma as a test runner, and building it in a way that it will work with RequireJS or Browserify.

---
+++ karma.conf.js
@@ -0,0 +1,54 @@
+// Loading dependencies.
+var _ = require('underscore'),
+    globalSettings = require('./run/_global');
+
+// Browserify settings that both RequireJS and Browserify agree on.
+var defaultSettings = {
+    basePath: '',
+    exclude: [],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    singleRun: true
+};
+
+// Create an object containing implementation specific settings.
+if (globalSettings.moduleFormat === 'browserify') {
+    var specificSettings = {
+        frameworks: ['mocha', 'browserify', 'chai', 'sinon'],
+        preprocessors: {
+            'js/tests/**/*.spec.js': ['browserify']
+        },
+        browserify: {
+            transform: ['hbsfy'],
+            watch: false
+        },
+        files: [
+            'js/tests/**/*.spec.js'
+        ]
+    };
+} else {
+    var specificSettings = {
+        frameworks: ['mocha', 'requirejs', 'chai', 'sinon'],
+        files: [
+            { pattern: 'js/libs/**/*.js', included: false },
+            { pattern: 'js/src/**/*.js', included: false },
+            { pattern: 'js/tests/**/*.spec.js', included: false },
+            'js/tests/requirejs-main.js'
+        ]
+    };
+}
+
+// Create an object which combines the defaults with the implementation specific.
+var combinedSettings = _.extend({}, defaultSettings, specificSettings);
+
+module.exports = function(config) {
+    'use strict';
+
+    // Appending this setting in the closure as it has access to the config object.
+    combinedSettings.logLevel = config.LOG_INFO;
+
+    config.set(combinedSettings);
+};
