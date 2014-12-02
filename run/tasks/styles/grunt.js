5e566698a649be1f2a134ce4d4b8ff180757579d
Anthony Hastings <anthony@rehabstudio.com>
2014-12-02 12:59:52 +0000

    - Adding grunt version of the styles revisions.

--- run/tasks/styles/grunt.js
+++ run/tasks/styles/grunt.js
@@ -8,22 +8,12 @@
  */
 
 var grunt = require('grunt'),
-    args = require('yargs').argv,
     common = require('./_common');
 
 grunt.loadNpmTasks('grunt-contrib-sass');
 grunt.loadNpmTasks('grunt-autoprefixer');
 
 grunt.config.set('sass', {
-    dist: {
-        files: [{
-            expand: true,
-            cwd: common.srcDirectory,
-            src: common.srcPath,
-            dest: common.destPath,
-            ext: '.css'
-        }]
-    },
     options: common.sassSettings
 });
 
@@ -41,5 +31,24 @@ grunt.config.set('autoprefixer', {
 });
 
 grunt.registerTask('styles', function() {
+    for (var index = 0, length = common.bundles.length; index < length; index++) {
+        // Generate a unique build key for this row of the bundles array.
+        var uniqueBuildKey = 'build-' + index,
+            thisBundle = common.bundles[index];
+
+        // Generate paths for this row.
+        var destPath = common.destPath + thisBundle.fileName + '.css',
+            sourcePath = thisBundle.srcPath + thisBundle.fileName + '.scss',
+            buildObject = {};
+
+        // Apply the generated dest / source paths to the build object.
+        buildObject.files = {};
+        buildObject.files[destPath] = sourcePath;
+
+        // Apply this specific build configuration to the overall SASS settings.
+        grunt.config.set('sass.' + uniqueBuildKey, buildObject);
+    }
+
+    // Compile SASS into CSS then prefix and save.
     grunt.task.run(['sass', 'autoprefixer']);
 });
