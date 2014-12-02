fd8e44118acab450dcc0e73d027cd9317d06a9a0
Anthony Hastings <anthony@rehabstudio.com>
2014-12-02 16:57:03 +0000

    - Adding test method.

---
+++ run/tasks/test/_common.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    configPath: __dirname + '/../../../karma.conf.js'
+};
---
+++ run/tasks/test/gulp.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ *  Starts the test runner which in turn loads test frameworks,
+ *  assertion libraries and then executes tests.
+ *
+ *  Example Usage:
+ *  gulp test
+ *  gulp test --watch
+ */
+
+var gulp = require('gulp'),
+    args = require('yargs').argv,
+    common = require('./_common'),
+    karma = require('karma');
+
+gulp.task('test', function(done) {
+    var karmaSettings = {
+        configFile: common.configPath
+    };
+
+    if (args.watch) {
+        karmaSettings.autoWatch = true;
+        karmaSettings.singleRun = false;
+    }
+
+    karma.server.start(karmaSettings, done);
+});
