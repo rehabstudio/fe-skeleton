add55f9bf12ff5275a92698c13b5f74739a8b12b
Anthony Hastings <ar.hastings@gmail.com>
2015-07-12 23:50:37 +0100

    Updating styles documentation.

--- run/tasks/styles/index.js
+++ run/tasks/styles/index.js
@@ -3,15 +3,6 @@
 /**
  *  Compiles, minifies and prefixes SASS.
  *
- *  Until Node-Sass starts using libsass 3.2 sourcemaps are fundamentally broken:
- *  https://github.com/sass/node-sass/issues/619
- *  https://github.com/sass/libsass/issues/837
- *  https://github.com/sass/libsass/pull/792
- *  https://github.com/sass/libsass/pull/910
- *
- *  libsass 3.2 progress:
- *  https://github.com/sass/libsass/milestones/3.2
- *
  *  Example Usage:
  *  gulp styles
  */
