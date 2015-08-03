a25ce49db143ad45cb47f4e204a56d314400023c
Anthony Hastings <anthony@rehabstudio.com>
2015-08-03 17:02:14 +0100

    Adding prompts into the terminal to inform a user if their commit has failed due to linting or formatting.

--- run/tasks/jscs/index.js
+++ run/tasks/jscs/index.js
@@ -14,15 +14,24 @@
  *
  *  Example Usage:
  *  gulp jscs
+ *  gulp jscs --via-commit
  *  gulp jscs --filePath js/src/app.js
  */
 
 var gulp = require('gulp'),
     args = require('yargs').argv,
+    chalk = require('chalk'),
     common = require('./_common'),
     jscs = require('gulp-jscs');
 
-gulp.task('jscs', function () {
+gulp.task('jscs', function() {
     return gulp.src(common.buildSources(args.filePath))
-        .pipe(jscs());
+        .pipe(jscs())
+        .on('error', function() {
+            if (args.viaCommit) {
+                setTimeout(function() {
+                    console.log(chalk.bgRed.white("\n FE Skeleton: Your commit has failed! Fix errors then try to re-commit."));
+                }, 0);
+            }
+        });
 });
