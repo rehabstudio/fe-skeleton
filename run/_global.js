cf19f958bc021ffe5bcae6cdb5535c52bece52d1
Anthony Hastings <anthony@rehabstudio.com>
2015-11-03 17:07:23 +0000

    Moving script task configuration over to global.

--- run/_global.js
+++ run/_global.js
@@ -88,6 +88,38 @@ module.exports = {
                 https: false,
                 open: true
             }
+        },
+        scripts: {
+            /**
+             *  An array of objects symbolising bundles requiring built.
+             *
+             *  Bundle Options:
+             *  `srcPath` - Folder where source files can be found. Relative to `package.json` and `gulpfile.js`.
+             *  `fileName` - File within `srcPath` which is the bundle starting point.
+             *  `excludes` - Any package names that need to be excluded from bundle i.e. `jquery`.
+             *
+             *  Example Bundles:
+             *  { srcPath: './js/src/', fileName: 'homepage', excludes: [] },
+             *  { srcPath: './js/src/', fileName: 'contact-us', excludes: [] }
+             */
+            bundles: [
+                { srcPath: './js/src/', fileName: 'index', excludes: [] }
+            ],
+
+            // Gets appended to a bundles `fileName` and placed into `outputFolder`.
+            buildFileSuffix: '.min.js',
+
+            // Where to place the built bundles. Is prefixed with `destPath` from global settings.
+            outputFolder: './js/',
+
+            // Settings for UglifyJS2.
+            uglifySettings: {
+                compress: {
+                    drop_console: false,
+                    drop_debugger: false,
+                    warnings: false
+                }
+            }
         }
     }
 
