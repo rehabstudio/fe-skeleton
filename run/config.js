b1b9a47e667b504687ffce7e7bffc9583c69398e
Anthony Hastings <anthony@rehabstudio.com>
2016-07-05 10:54:55 +0100

    Moving sourcemap settings into styles options as they're now CSS specific.

--- run/config.js
+++ run/config.js
@@ -3,44 +3,6 @@
 module.exports = {
 
     /**
-     * Settings for sourcemaps across JS and CSS bundles.
-     *
-     * @type {Object}
-     */
-    sourcemapOptions: {
-
-        /**
-         * Sourcemaps are built externally but there is two choices.
-         * Source files can be embedded inside the map itself, or,
-         * the source files can be referenced by the map and loaded
-         * whenever you try to click line numbers in your dev tools.
-         *
-         * If your source files are not being served via a web server
-         * then stick to using `External_EmbeddedFiles`.
-         *
-         * Potential Values:
-         * 'External_EmbeddedFiles'
-         * 'External_ReferencedFiles'
-         *
-         * @type {String}
-         */
-        type: 'External_EmbeddedFiles',
-
-        /**
-         * Sets the root path of where source files are hosted. This
-         * path is relative to the source map. If you have sources in
-         * different subpaths, an absolute path (from the domain root)
-         * pointing to the source file root is recommended.
-         *
-         * NOTE: Only needs to be set for 'External_ReferencedFiles'.
-         *
-         * @type {String}
-         */
-        sourceRoot: '/'
-
-    },
-
-    /**
      * Where built output (CSS, JS, HTML, fonts, images) should be
      * stored on the filesystem. Can either be an absolute path or
      * relative path to the location of the gulpfile.
@@ -95,8 +57,8 @@ module.exports = {
              * `outputFolder` (Optional) - Overrides `genericOutputFolder`. Is relative to `gulpfile.js`.
              *
              * Example Bundles:
-             * { sourceFilePath: './css/src/homepage.scss', outputFileName: 'homepage' },
-             * { sourceFilePath: './css/src/about.scss', outputFileName: 'main', outputFolder: './modules/about-page/css/' }
+             * {sourceFilePath: './css/src/homepage.scss', outputFileName: 'homepage'},
+             * {sourceFilePath: './css/src/about.scss', outputFileName: 'main', outputFolder: './modules/about-page/css/'}
              *
              * @type {Array}
              */
@@ -121,6 +83,44 @@ module.exports = {
             autoPrefixSettings: {
                 browsers: ['last 2 versions', 'iOS >= 7.1', 'Android >= 4'],
                 cascade: false
+            },
+
+            /**
+             * Settings for how sourcemaps should be output for bundles.
+             *
+             * @type {Object}
+             */
+            sourcemapOptions: {
+                /**
+                 * Sourcemaps are built externally but there is two choices.
+                 *
+                 * Source files can be embedded inside the map itself, or,
+                 * the source files can be referenced by the map and loaded
+                 * whenever you try to click line numbers in your dev tools.
+                 *
+                 * If your source files are not being served via a web server
+                 * or they aren't publicly accessible then stick to using
+                 * `External_EmbeddedFiles`.
+                 *
+                 * Potential Values:
+                 * 'External_EmbeddedFiles'
+                 * 'External_ReferencedFiles'
+                 *
+                 * @type {String}
+                 */
+                type: 'External_EmbeddedFiles',
+
+                /**
+                 * Sets the root path of where source files are hosted. This
+                 * path is relative to the source map. If you have sources in
+                 * different subpaths, an absolute path (from the domain root)
+                 * pointing to the source file root is recommended.
+                 *
+                 * NOTE: Only needs to be set for 'External_ReferencedFiles'.
+                 *
+                 * @type {String}
+                 */
+                sourceRoot: '/'
             }
         },
         scripts: {
